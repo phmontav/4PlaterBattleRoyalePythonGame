50e0e989ff7085bca353eb1db85f60824b63cd03
phmontav <phmontav@gmail.com>
2022-01-12 17:33:23 -0300

    inicial commit

---
+++ player.py
@@ -0,0 +1,11 @@
+class player:
+   
+    def __init__(self,nome,total):
+        self.nome = nome
+        self.life = 100
+        self.attack = 10.0 ## corrigit em battle tbm
+        self.defense = 1.0
+        self.id = total
+    def atacar(self):
+        pass
+
