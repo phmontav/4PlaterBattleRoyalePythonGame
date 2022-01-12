74c19350b32f0833f8bef301cb5508085ac3a2a7
phmontav <phmontav@gmail.com>
2022-01-12 19:09:45 -0300

    layout main screen

--- graficos/tela_principal.py
+++ graficos/tela_principal.py
@@ -2,7 +2,7 @@ from tkinter import *
 from PIL import Image
 root = Tk()
 WIDHT = 900
-HEIGHT = 600
+HEIGHT = 500
 #root.geometry("800x600+300+50")
 my_blue = "#48b8fa"
 root.title("Battle Royale")
@@ -13,13 +13,12 @@ WIDHT = big_fundo.width()
 root.geometry(str(WIDHT) + "x" + str(HEIGHT) + "+80+50")
 
 frame_up = Frame(root)
-frame_up.pack(fill="both",expand=True,side="top")
-
+frame_up.place(relheight=0.8,relwidth=1)
 bg = Label(frame_up,image=big_fundo)
 bg.place(x=0,y=0)
+frame_down = Frame(root,bg="gray",padx=10,relief=SUNKEN,border=10)
+frame_down.place(relheight=0.3,relwidth=1,rely=0.75)
 
-frame_down = Frame(root,padx=105,pady=10,bg="white")
-frame_down.pack(fill="both",expand=True,side="bottom")
 
 
 
