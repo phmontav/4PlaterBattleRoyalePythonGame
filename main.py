3c1a42d6480a40d1ea181e98952a6d50358808df
phmontav <phmontav@gmail.com>
2022-01-14 20:22:16 -0300

    check-point

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from pydoc import cli
 import sys
+from graficos.connection_screen import App
+from tkinter import *
+from PIL import Image
 
 #problema com path do python
 sys.path.insert(0,sys.path[0] + "/graficos")
-import tela_conexao
 import client
 
-usuario = tela_conexao.get_user()
-print(usuario)
-try:
-    object_socket = client.connect(usuario)
-except:
-    print("pau")
+root = Tk()
 
-try:
-    client.play(obj_socket=object_socket)
-except:
-    print("pau2")
+
+
+
+def startGui():
+    app = App(root)
+    root.mainloop()
+
+
+
+
+if __name__=="__main__":
+    startGui() 
